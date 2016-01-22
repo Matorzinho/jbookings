9e1aacfbb5fdd8247b2da8189f3e54e9052da1b7
DougB <dougbacelar@gmail.com>
2016-01-22 16:29:56 -0200

    lets use Materialize instead

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-	echo "hello";
+	// echo "hello";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Bookings</title>
+
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+	    <script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<h1>Bookings</h1>
+	</body>
+</html>
